f77c11ecf08b39853d09e08a52d51b9dddf52933
Your Name <you@example.com>
2019-09-02 22:33:09 -0300

    google form

---
+++ js/footstep.js
@@ -0,0 +1,62 @@
+
+
+(function () {
+
+    /**
+     * Remove acentos de caracteres
+     * @param  {String} stringComAcento [string que contem os acentos]
+     * @return {String}                 [string sem acentos]
+     */
+    function removerAcentos(newStringComAcento) {
+        var string = newStringComAcento;
+        var mapaAcentosHex = {
+            a: /[\xE0-\xE6]/g,
+            e: /[\xE8-\xEB]/g,
+            i: /[\xEC-\xEF]/g,
+            o: /[\xF2-\xF6]/g,
+            u: /[\xF9-\xFC]/g,
+            c: /\xE7/g,
+            n: /\xF1/g
+        };
+
+        for (var letra in mapaAcentosHex) {
+            var expressaoRegular = mapaAcentosHex[letra];
+            string = string.replace(expressaoRegular, letra);
+        }
+
+        return string;
+    }
+
+    $.get('https://uinames.com/api/', {
+        region: 'brazil'
+    }, (response) => {
+
+        let client_id = `${response.name} ${response.surname} ${new Date().getTime()}`;
+        client_id = client_id.replace(/ /g, '-').toLowerCase();
+        client_id = removerAcentos(client_id);
+
+        $('#google-form')
+            .attr(
+                'src',
+                `https://docs.google.com/forms/d/e/1FAIpQLSfh9lfSIJEJ-35aFELWsKVdfKnqz7MJmS4-ZDwNHXqcaEFdYA/viewform?embedded=true&entry.1535443542=${client_id}`
+            )
+
+        var trck = document.createElement('script');
+        trck.async = true;
+        trck.type = 'text/javascript';
+        trck.src = 'https://footstep.io/evc/static/js/trck.min.js';
+        var node = document.getElementsByTagName('script')[0];
+        node.parentNode.insertBefore(trck, node);
+        trck.onload = function () {
+            _trck.init({
+                client_id: client_id,
+                cookie_cfg: {
+                    name: '_trck', //optional; default: _trck
+                    domain: 'https://guilheeeeeeerme.github.io/footstep/'
+                }
+            });
+        }
+
+    });
+
+})();
