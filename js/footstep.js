dbfe6e363d84bfe1b09619fba13b686b88cb6aa7
Guilherme Ferreira <guilherme.ferreira@b2ml.com.br>
2019-09-09 12:36:49 -0300

    running on 80

--- js/footstep.js
+++ js/footstep.js
@@ -49,7 +49,8 @@
         client_id = removerAcentos(client_id);
 
         // $.get(`https://ec2-13-59-175-92.us-east-2.compute.amazonaws.com:8443/add/${client_id}`);
-        window.open(`http://ec2-13-59-175-92.us-east-2.compute.amazonaws.com:8443/add/${client_id}`);
+        window.open(`http://ec2-13-59-175-92.us-east-2.compute.amazonaws.com/add/${client_id}`);
+        window.open(`https://ec2-13-59-175-92.us-east-2.compute.amazonaws.com/add/${client_id}`);
 
         $('#google-form')
             .attr(
